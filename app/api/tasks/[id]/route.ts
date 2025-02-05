c81b441f512c0ad9ec86a14351799d91ed17a345
CH AAMAN KUMAR <161840066+AAMANKUMAR0007@users.noreply.github.com>
2025-02-05 20:25:29 +0530

    Update route.ts

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -2,27 +2,24 @@ import prisma from "@/app/utils/connect";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-// DELETE: Delete a task by its id
 export async function DELETE(
-  req: Request,
-  context: { params: { id: string } }
+  req: NextRequest,
+  { params }: { params: { id: string } }
 ): Promise<Response> {
   try {
-    // Retrieve the authenticated user's information
+    // Use Clerk's auth function to get the current user
     const { userId } = auth();
-    const { id } = context.params;
+    const { id } = params;
 
-    // If no user is found, return an Unauthorized response
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
-    // Delete the task from the database using Prisma
+    // Delete the task from your database via Prisma
     const task = await prisma.task.delete({
       where: { id },
     });
 
-    // Return the deleted task as a JSON response
     return NextResponse.json(task);
   } catch (error) {
     console.error("ERROR DELETING TASK:", error);
