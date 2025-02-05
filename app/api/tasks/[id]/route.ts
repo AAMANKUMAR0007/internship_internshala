495e7adc36a85794936248aeeab6c5136643f21c
Aaman <chaamankumar1231@gmail.com>
2025-02-05 20:04:28 +0530

    code upload 2

--- app/api/tasks/[id]/route.ts
+++ app/api/tasks/[id]/route.ts
@@ -1,28 +1,34 @@
 import prisma from "@/app/utils/connect";
-import { auth } from "@clerk/nextjs";
+import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
+// DELETE: Delete a task by its id
 export async function DELETE(
   req: Request,
-  { params }: { params: { id: string } }
-) {
+  context: { params: { id: string } }
+): Promise<Response> {
   try {
+    // Retrieve the authenticated user's information
     const { userId } = auth();
-    const { id } = params;
+    const { id } = context.params;
 
+    // If no user is found, return an Unauthorized response
     if (!userId) {
       return new NextResponse("Unauthorized", { status: 401 });
     }
 
+    // Delete the task from the database using Prisma
     const task = await prisma.task.delete({
-      where: {
-        id,
-      },
+      where: { id },
     });
 
+    // Return the deleted task as a JSON response
     return NextResponse.json(task);
   } catch (error) {
-    console.log("ERROR DELETING TASK: ", error);
-    return NextResponse.json({ error: "Error deleting task", status: 500 });
+    console.error("ERROR DELETING TASK:", error);
+    return NextResponse.json(
+      { error: "Error deleting task", status: 500 },
+      { status: 500 }
+    );
   }
 }
